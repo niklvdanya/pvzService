af3373d501599c1be8ea3e4fcb3fd587cec7e421
Daniil Nikolaev <quanttlight@gmail.com>
2025-06-07 17:07:39 +0300

    refactor: dependency inversion

--- internal/adapter/grpc/server.go
+++ internal/adapter/grpc/server.go
@@ -1,17 +1,29 @@
 package server
 
 import (
-	"gitlab.ozon.dev/safariproxd/homework/internal/app"
+	"gitlab.ozon.dev/safariproxd/homework/internal/domain"
 	"gitlab.ozon.dev/safariproxd/homework/pkg/api"
 	"google.golang.org/grpc"
 )
 
-type OrdersServer struct {
-	api.UnimplementedOrdersServiceServer
-	service *app.PVZService
+type IOrderService interface {
+	AcceptOrder(req domain.AcceptOrderRequest) (float64, error)
+	ReturnOrderToDelivery(orderID uint64) error
+	IssueOrdersToClient(receiverID uint64, orderIDs []uint64) error
+	ReturnOrdersFromClient(receiverID uint64, orderIDs []uint64) error
+	GetReceiverOrders(receiverID uint64, inPVZ bool, lastN, page, limit uint64) ([]*domain.Order, uint64, error)
+	GetReceiverOrdersScroll(receiverID uint64, lastID, limit uint64) ([]*domain.Order, uint64, error)
+	GetReturnedOrders(page, limit uint64) ([]*domain.Order, uint64, error)
+	GetOrderHistory() ([]*domain.Order, error)
+	ImportOrders(orders []domain.OrderToImport) (uint64, error)
 }
 
-func NewOrdersServer(service *app.PVZService) *OrdersServer {
+type OrdersServer struct {
+	api.UnimplementedOrdersServiceServer
+	service IOrderService
+}
+
+func NewOrdersServer(service IOrderService) *OrdersServer {
 	return &OrdersServer{
 		service: service,
 	}
