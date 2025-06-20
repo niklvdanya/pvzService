144258c01fcd4e4aec2e44cd338b474029ba46bb
Daniil Nikolaev <quanttlight@gmail.com>
2025-06-20 19:47:44 +0300

    refactor: remove receivers table from migration

--- migrations/001_init.sql
+++ migrations/001_init.sql
@@ -1,17 +1,13 @@
 -- +goose Up
-CREATE TABLE receivers (
-    id            BIGSERIAL PRIMARY KEY
-);
-
 CREATE TABLE package_types (
-    code          TEXT PRIMARY KEY,          
+    code          TEXT PRIMARY KEY,
     max_weight    NUMERIC(10,2) NOT NULL,
     extra_price   NUMERIC(10,2) NOT NULL
 );
 
 CREATE TABLE orders (
     id               BIGINT       PRIMARY KEY,
-    receiver_id      BIGINT       NOT NULL REFERENCES receivers(id),
+    receiver_id      BIGINT       NOT NULL,
     status           SMALLINT     NOT NULL,
     expires_at       TIMESTAMPTZ  NOT NULL,
     accept_time      TIMESTAMPTZ  NOT NULL,
@@ -27,4 +23,3 @@ CREATE INDEX idx_orders_receiver_status ON orders (receiver_id, status);
 DROP INDEX IF EXISTS idx_orders_receiver_status;
 DROP TABLE IF EXISTS orders;
 DROP TABLE IF EXISTS package_types;
-DROP TABLE IF EXISTS receivers;
